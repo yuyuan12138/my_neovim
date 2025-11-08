23936bf0b3707b701ae229cf61ac433bdb3c2ddf
yuyuan <yuyuan12138x@gmail.com>
2025-11-08 23:50:27 +0800

    rm init.lua

--- lua/plugin-config/markdown.lua
+++ lua/plugin-config/markdown.lua
@@ -2,7 +2,7 @@ return {
     {
         'MeanderingProgrammer/render-markdown.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, 
-        config = function() require('competitest').setup({
+        config = function() require('render-markdown').setup({
         }) end,
     }
 }
