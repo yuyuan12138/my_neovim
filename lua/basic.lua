0686568055af5914745d32f18a3e1515d53f7dba
yuyuan <yuyuan12138x@gmail.com>
2025-11-10 14:13:12 +0800

    update tree

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,3 +1,9 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
 -- 设置默认文件格式为 Unix (LF)
 vim.opt.fileformat = "unix"
 
--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,29 +1,7 @@
--- ================== 编译运行（Windows PowerShell） ==================
--- 注意：Windows 下需要 .\ 前缀执行 exe 文件
--- F5: 仅编译（检查错误）
-vim.keymap.set('n', '<F5>', function()
-    vim.cmd('silent w')  -- 保存文件
-    local output = vim.fn.expand('%:p:r') .. '.exe'
-    vim.cmd('!g++ % -o %<.exe -std=c++23 -O2 -Wall -DLOCAL -I E:/OneDrive/Project/cp/debug')
-  end)
-
 -- nvim-tree
 vim.keymap.set('n', '<A-m>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 
--- CompetiTest
-vim.keymap.set('n', "<C-A-b>", function()
-  vim.cmd('silent w')  
-  vim.cmd('CompetiTest run')
-end)
-vim.keymap.set('n', "<C-b>", function()
-    vim.cmd('silent w')
-    vim.cmd('CompetiTest show_ui')
-end)
-
-vim.keymap.set('n', '<F2>', ':CompetiTest receive testcases<CR>')
-
 local opt = {noremap = true, silent = true}
-
 -- 缩进代码
 vim.keymap.set('v', '<', '<gv', opt)
 vim.keymap.set('v', '>', '>gv', opt)
@@ -37,11 +15,5 @@ vim.keymap.set('v', 'p', '"_dP', opt)
 vim.keymap.set('n', 'q', ':q<CR>', opt)
 vim.keymap.set('n', '<C-R>s', ':w<CR>', {noremap = true})
 vim.keymap.set('n', 'qq', ':q!<CR>', opt)
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = {"*.c","*.cpp","*.h","*.hpp"},
-  callback = function() clang_format_buffer() end,
-})
-
 -- INSERT
 vim.keymap.set('i', 'jk', '<ESC>', opt)
